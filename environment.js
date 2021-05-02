6eef3579786078a8a051dfd33720ff0e76f13c6d
Aditya Chandak <adityachandak287@gmail.com>
2021-05-02 16:19:22 +0530

    Updated env sample and created environment.js to store constants

---
+++ environment.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const environment = {
+  CALENDAR_BY_DISTRICT_BASE_URL:
+    "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict",
+  FEE_TYPE: process.env.FEE_TYPE || "Free,Paid",
+  MIN_AGE_LIMIT: process.env.MIN_AGE_LIMIT ? +process.env.MIN_AGE_LIMIT : 18,
+  NODE_ENV: process.env.NODE_ENV || "prod",
+  SLACK_HOOK_URL: process.env.SLACK_HOOK_URL,
+  DISTRICT_ID: process.env.DISTRICT_ID || "395",
+};
+
+module.exports = environment;
